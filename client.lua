c15053dfc9608cbae32921ba96f1e53dfdef5061
M0_kii <M0_kii@proton.me>
2026-03-31 13:14:58 +0300

    Made the shutters closed by defualt

--- client.lua
+++ client.lua
@@ -1,14 +1,13 @@
 local interiorID = nil
-
 local shutters = {
-    {name = 'shutter01', job = 'smoothie', coords = vector3(-507.01, -709.02, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter02', job = 'burger', coords = vector3(-506.95, -714.94, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter03', job = 'sandwich', coords = vector3(-507.05, -720.76, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter04', job = 'tacos', coords = vector3(-515.17, -721.67, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter05', job = 'pizza', coords = vector3(-518.51, -717.0, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter06', job = 'sushisushi', coords = vector3(-521.17, -712.0, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter07', job = 'coffee', coords = vector3(-523.37, -706.63, 33.67), size = vec3(1,1,2), rot = 0},
-    {name = 'shutter08', job = 'noodle', coords = vector3(-526.58, -701.09, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter01', job = 'smoothie', coords = vector3(-506.50, -709.02, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter02', job = 'burger', coords = vector3(-506.50, -714.84, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter03', job = 'sandwich', coords = vector3(-506.55, -720.76, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter04', job = 'tacos', coords = vector3(-515.87, -722.10, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter05', job = 'pizza', coords = vector3(-518.90, -717.20, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter06', job = 'sushisushi', coords = vector3(-521.87, -712.30, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter07', job = 'coffee', coords = vector3(-524, -706.90, 33.67), size = vec3(1,1,2), rot = 0},
+    {name = 'shutter08', job = 'noodle', coords = vector3(-525.30, -701.09, 33.67), size = vec3(1,1,2), rot = 0},
     {name = 'shutter09', job = 'donerkebab', coords = vector3(-526, -695.44, 33.67), size = vec3(1,1,2), rot = 0},
     {name = 'shutter10', job = 'hotdogs', coords = vector3(-526.56, -689.49, 33.67), size = vec3(1,1,2), rot = 0},
 }
@@ -17,7 +16,13 @@ CreateThread(function()
     RequestIpl('3dp_snrbuns_milo')
     Wait(2000)
     interiorID = GetInteriorAtCoords(-515.792969, -706.0899, 38.7752)
-    if IsValidInterior(interiorID) then RefreshInterior(interiorID) end
+    if IsValidInterior(interiorID) then
+        for _, sh in ipairs(shutters) do
+            sh.state = true
+            EnableInteriorProp(interiorID, sh.name)
+        end
+        RefreshInterior(interiorID)
+    end
 end)
 
 for _, sh in ipairs(shutters) do
